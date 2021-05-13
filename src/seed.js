34e627dc86ddce3364bfd074fc756b6fb8e0219e
Vikas0786 <vthakur412000@gmail.com>
2021-05-13 17:33:44 +0530

    Dashboard and Profile page updated

--- src/seed.js
+++ src/seed.js
@@ -4,9 +4,9 @@ export function seedDatabase(firebase) {
   const users = [
     {
       userId: "HjOUYgWjoka0DG3LRAuTrvO4ajr2",
-      username: "vikasthakur0786",
-      fullName: "Vikas Thakur",
-      emailAddress: "vikas786@gmail.com",
+      username: "karl",
+      fullName: "Karl Hadwen",
+      emailAddress: "karlhadwen@gmail.com",
       following: ["2"],
       followers: ["2", "3", "4"],
       dateCreated: Date.now(),
